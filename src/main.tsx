454d3cafac97799f0eaa15bd66c554894bf3ff23
malsaslamcraft97 <aslam@incubyte.co>
2026-04-17 14:56:07 +0530

    feat: add Suspense fallback for async UI handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.scss';
-import { AppProvider } from './context/AppProvider';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
+import { AppProvider } from "./context/AppProvider";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
     </AppProvider>
   </React.StrictMode>,
 );
